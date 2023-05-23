18950c3e160c5b9b4862b0870035b3eba1b38539
liuhongwei <769591656@qq.com>
2023-05-23 15:57:38 +0800

    修改分析界面curve_ids为空查询全部

--- blueprints/curve_blueprint.py
+++ blueprints/curve_blueprint.py
@@ -132,7 +132,7 @@ def delete_curve_by_id():
     post_data = json.loads(args_str)
 
     current_app.logger.info('调用query方传过来的参数是', post_data)
-    curve_ids = post_data.get('curve_ids')
+    curve_ids = post_data.get("curve_ids")
     file_list = set()
     for curve_id in curve_ids:
         file_name = get_file_name_by_curve_id(curve_id)
@@ -150,7 +150,7 @@ def delete_curve_by_file():
     args_str = request.form.get("args", "{}")
     post_data = json.loads(args_str)
     current_app.logger.info('调用query方传过来的参数是', post_data)
-    file_list = post_data.get('file_list')
+    file_list = post_data.get("file_list")
     delete_curve_with_file_list(file_list)
 
     return response_object
